63addb62e7d76ed40f38267d4eb8be28688bb37b
alumno <alumno>
2019-02-11 19:13:41 -0300

    ejercicios tema 1, 1 al 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,14 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	var primerNumero;
+	var resultado;
+
+	primerNumero = document.getElementById("importe").value;
+	resultado = document.getElementById("resultado").value;
+
+	primerNumero = parseInt(primerNumero);
+	resultado = (primerNumero * 25 / 100);
+
+	alert("el resultado es:" +resultado);
 }
