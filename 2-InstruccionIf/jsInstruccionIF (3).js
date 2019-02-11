b0ff287cc215bfcde597fe3364e0632efc0e6ee8
alumno <alumno>
2019-02-11 19:55:11 -0300

    ejercicio 1 al 3 tema 2

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -2,5 +2,23 @@ function mostrar()
 {
 //tomo la edad  
 
+var primerNumero;
+
+	primerNumero = parseInt(primerNumero)
+
+	primerNumero = edad.value;
+
+	if (primerNumero >= 18)
+	
+	{ alert("usted es mayor de edad");
+
+	}
+
+	if(primerNumero <= 17)
+
+		{ alert("usted es menor de edad, pendejo");
+
+	}
+
 
 }//FIN DE LA FUNCIÓN
