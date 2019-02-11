6d17c55370c20139ae5cf8189866588206c68e0c
alumno <alumno>
2019-02-11 20:38:14 -0300

    ejercicio 5

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -5,15 +5,15 @@ function mostrar()
 
 	var primerNumero;
 
-	primerNumero = parseInt(primerNumero)
+	primerNumero = parseInt(primerNumero);
 
 	primerNumero = edad.value;
 
-	if (primerNumero >=13 && primerNumero <= 17 )
+	if (primerNumero >=13 && (primerNumero <= 17) )
 
 		{ alert("usted es un adolescente");
 
-}
-
+		}
+//otra forma es escribiendo && var y el valor
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -3,4 +3,17 @@ function mostrar()
 //tomo la edad  
 
 
+	var primerNumero;
+
+	primerNumero = parseInt(primerNumero);
+
+	primerNumero = edad.value;
+
+	if (primerNumero >=17 || primerNumero <= 12 )
+
+		{ alert("usted no es un adolescente");
+
+		}
+
+
 }//FIN DE LA FUNCIÓN
