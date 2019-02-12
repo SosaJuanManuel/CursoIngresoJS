f0e7193c1d7169eb9e5ebfd092b0237fcbf01b47
alumno <alumno>
2019-02-11 21:03:50 -0300

    ejercicio 6 tema 2

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -2,6 +2,30 @@ function mostrar()
 {
 //tomo la edad  
 
+var primerNumero;
+
+	primerNumero = parseInt(primerNumero);
+
+	primerNumero = edad.value;
+
+	if (primerNumero >=18 )
+		{ alert("usted es mayor de edad");
+
+		}
+
+	if (primerNumero >12 && primerNumero < 18 ) 
+	{
+
+		alert("usted es un adolescente");
+
+	}	
+
+	if (primerNumero <13)
+		{
+
+			alert("usted es menor de edad");
+
+		}
 
 
 
--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -3,6 +3,21 @@ function mostrar()
 //tomo la edad  
 
 	
+	var laEdad;
+	var relacion;
+
+	laEdad.parseint(laEdad);
+	laEdad = edad.value;
+
+	relacion = estadoCivil.value
+
+	if(laEdad <18 && relacion = soltero)
+		{
+			alert( 'Es muy pequeño para NO ser soltero.')
+
+
+		}
+
 
 
 }//FIN DE LA FUNCIÓN
